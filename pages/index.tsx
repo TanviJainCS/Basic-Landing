00624242e97512226d0d2c0f0931b1d354cbc9e8
TanviJainCS <tanvi.jain@contentstack.com>
2025-05-27 03:06:54 +0530

    feat: added SEO details and bg images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,63 @@
 import { GetStaticProps } from 'next';
 import Stack, { fetchBlogs } from '@/lib/contentstack';
-import { Banner } from '../components/Banner';
+import { Banners } from '../components/Banner';
 import { BlogSection } from '../components/BlogSection';
 import { CTA } from '@/components/cta';
+import BG1 from '../public/Background1.svg';
+import BG2 from '../public/Background2.svg';
+import Image from 'next/image';
+import Head from 'next/head';
+import { ComponentBlock, PageProps } from '@/type/contenttype';
 
-type Blog = {
-  title: string;
-  url: string;
-  date: string;
-  blog_image: { url: string };
-  body: string;
-};
-
-type ComponentBlock =
-  | { hero_banner: { banner_title: string; banner_description: string } }
-  | { blog: { title: string; featured_blog?: Blog[] } }
-  | { cta: { cta_title: string; cta_description: string; link?: any; link_get_started?: any } };
-
-type PageProps = {
-  page: {
-    title: string;
-    page_components: ComponentBlock[];
-  };
-  blogs: Blog[];
-};
 
 export default function Home({ page, blogs }: PageProps) {
   return (
+        <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>Thank you</title>
+        <meta name="description" content="Thank you for your inquiry. A member of our sales team will be in touch to schedule a demo in the next business day." />
+        <meta property="og:image" content="img" />
+        <meta name="robots" content="noindex, nofollow" />
+        <link rel="canonical" href="https://www.contentstack.com/request-demo/thank-you" />
+
+      </Head>
     <main style={{ fontFamily: "'Inter', sans-serif" }}>
+
+      <div className="bannerBackgroundWrapper">
+        <Image
+          src={BG1}
+          alt="Banner Background"
+          fill
+          style={{ objectFit: 'contain', objectPosition: 'top right' }}
+        />
+      </div>
+
+    
+    
       {page.page_components.map((block, i) => {
         if ('hero_banner' in block) {
-          return <Banner key={i} {...block.hero_banner} />;
+          //console.log(block.hero_banner);
+          return <Banners key={i} {...block.hero_banner} />;
         }
         if ('blog' in block) {
-          return <BlogSection key={i} blogs={blogs} />; 
+          return <BlogSection key={i} blogs={blogs} />;
         }
         if ('cta' in block) {
           return <CTA key={i} {...block.cta} />;
         }
         return null;
       })}
+        <div className="bgWrapper">
+        <Image
+          src={BG2}
+          alt="CTA Background"
+          fill
+         style={{ objectFit: 'cover', objectPosition:'bottom' }}
+        />
+      </div>
     </main>
+    </>
   );
 }
 
@@ -53,6 +70,15 @@ export const getStaticProps: GetStaticProps = async () => {
 
   const blogs = await fetchBlogs(); 
   const [entry] = pageResult[0];
+  // entry.page_components.forEach((component: ComponentBlock) => {
+  //   if ('hero_banner' in component) {
+  //     console.log('Hero Banner Block:', component.hero_banner); 
+  //     if ((component.hero_banner as any).uid) {
+  //       console.log('Hero Banner UID:', (component.hero_banner as any).uid);
+  //     }
+  //         }
+  // });
+
 
   return {
     props: {
